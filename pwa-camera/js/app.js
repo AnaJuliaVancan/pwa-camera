90bc2c85e992dad001c2d4f41d66cceb81df9b81
AnaJuliaVancan <anajuliavancan@gmail.com>
2022-10-26 17:59:38 -0300

    Style e app adicionados.

--- pwa-camera/js/app.js
+++ pwa-camera/js/app.js
@@ -4,3 +4,30 @@ window.addEventListener('load', e => {
         navigator.serviceWorker.register("./sw.js");
     }
 }); 
+
+var constraints = {vdeo: {facingMode:"user"}, audio: false}
+
+const cameraView = document.querySelector("#camera--view")
+cameraOutput = document.querySelector("#camera--output")
+cameraSensor = document.querySelector("#camera--sensor")
+cameraTrigger = document.querySelector("#camera--trigger")
+
+function cameraStart(){
+    navigator.mediaDevices
+    .getUserMedia(constrains)
+    .then(function(stream){
+        track = stream.getTracks()[0];
+        cameraView.srcObjects = stream;
+    })
+    .catch(function(error) {
+        console.error("Oops. Algo está errado.", error);
+    });
+}
+
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp")
+};
+window.addEventListener("load", cameraStart,false)
